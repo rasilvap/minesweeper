88b71bbae858fc8d1e076879faf9361b739ed27f
obarra <barraomar12@gmail.com>
2020-11-22 09:25:17 -0300

    :feat init client java spring boot thymeleaf

---
+++ minesweeper-client/src/main/java/com/obarra/minesweeperclient/MinesweeperClientApplication.java
@@ -0,0 +1,13 @@
+package com.obarra.minesweeperclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperClientApplication.class, args);
+	}
+
+}
---
+++ minesweeper-client/src/test/java/com/obarra/minesweeperclient/MinesweeperClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obarra.minesweeperclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
