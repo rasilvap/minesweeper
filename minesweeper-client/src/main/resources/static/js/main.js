e0df85ac07c24f43ecc4db28fbc7d03a7efb8ef1
obarra <barraomar12@gmail.com>
2020-11-23 12:57:41 -0300

    refactor: clean static files

--- minesweeper-client/src/main/resources/static/js/main.js
+++ minesweeper-client/src/main/resources/static/js/main.js
@@ -1,15 +1,15 @@
 function clickSq(event, thisTile) {
 	if (!event) event = window.event;
-debugger;
-    if (!event.shiftKey && event.which === 1 && event.button === 0) {
-    	play(thisTile.parentNode.rowIndex -1, thisTile.cellIndex)
-    	console.log("Left Click")
+	debugger;
+	if (!event.shiftKey && event.which === 1 && event.button === 0) {
+		play(thisTile.parentNode.rowIndex - 1, thisTile.cellIndex)
+		console.log("Left Click")
 	} else if (!event.shiftKey && event.which === 3 && event.button === 2) {
-		mark(thisTile.parentNode.rowIndex -1, thisTile.cellIndex)
+		mark(thisTile.parentNode.rowIndex - 1, thisTile.cellIndex)
 		console.log("Right Click")
 	} else if (event.shiftKey && event.which === 3 && event.button === 2) {
-   		console.log("Shift Right Click")
-     }  else {
+		console.log("Shift Right Click")
+	} else {
 		console.log("Invalid movement")
 	}
 
@@ -17,18 +17,18 @@ debugger;
 }
 
 function play(row, column) {
-executeAnchor(`${basePath}/play?row=${row}&column=${column}`)
+	executeAnchor(`${basePath}/play?row=${row}&column=${column}`)
 }
 
 function mark(row, column) {
-executeAnchor(`${basePath}/mark?row=${row}&column=${column}`)
+	executeAnchor(`${basePath}/mark?row=${row}&column=${column}`)
 }
 
 
 
 function executeAnchor(url) {
-  document.getElementById("myAnchor").href = url
-  window.location = document.getElementById("myAnchor").href;
+	document.getElementById("myAnchor").href = url
+	window.location = document.getElementById("myAnchor").href;
 }
 
 
@@ -40,5 +40,5 @@ function buildBasePath() {
 	let contextRoot = arr[3];
 
 	var basePath = `${protocol}//${hostPort}/${contextRoot}`
-  return basePath;
+	return basePath;
 }
