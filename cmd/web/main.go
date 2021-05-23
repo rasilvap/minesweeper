5d627a36cab80c021a970c77eb39355d4ca10906
obarra-dev <barra_omar@hotmail.com>
2021-05-23 19:45:30 -0300

    remove custom log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,26 +9,16 @@ import (
 	"minesweeper-API/container"
 	"minesweeper-API/models"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	_ "github.com/pdrum/swagger-automation/docs" // This line is necessary for go-swagger to find your docs!
 )
 
-var (
-	 LoggerInfo  *log.Logger
-	 LoggerError *log.Logger
-)
-
 func main() {
-	file, _ := os.OpenFile("log.txt", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
-
-	LoggerInfo = log.New(file, "INFO: ", log.LUTC|log.Lmicroseconds|log.Lshortfile )
-	LoggerError = log.New(file, "Error: ", log.LUTC|log.Lmicroseconds|log.Lshortfile )
 	env := flag.String("env", "dev", "Execution environment")
 	flag.Parse()
-	LoggerInfo.Printf("Starting application server - %s\n", *env)
+	log.Printf("Starting application server - %s", *env)
 
 	cfg := config.BuildConfig(*env)
 	c := container.New(cfg)
