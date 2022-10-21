710534737b74639d53fb89bfca96ab19f47bfb04
obarra <omarbarra@pop-os.localdomain>
2022-10-21 11:40:03 -0300

    add errors3

--- cmd/web/handlers/handle_health.go
+++ cmd/web/handlers/handle_health.go
@@ -24,3 +24,21 @@ func HandleHealth(w http.ResponseWriter, _ *http.Request) {
 		return
 	}
 }
+
+func HandleHealthSS(w http.ResponseWriter, _ *http.Request) {
+	js, err := json.Marshal(map[string]interface{}{
+		"name": "minesweeperAPI",
+		"info": "It is ok",
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(js)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
