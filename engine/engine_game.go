b37619aafd3b4ccd7217c9ece46f417983e70f03
obarra <barra_omar@hotmail.com>
2022-11-08 16:38:57 -0300

    Update engine_game.go

--- engine/engine_game.go
+++ engine/engine_game.go
@@ -12,7 +12,7 @@ type game struct {
 	minesWeeperEngine MinesWeeper
 }
 
-//TODO validate with pprof this with pointer cxxx
+//TODO validate with pprof this with pointer ggf
 func NewGame(gameDS datasource.Game, minesWeeperEngine MinesWeeper) Game {
 	return game{
 		gameDS:            gameDS,
